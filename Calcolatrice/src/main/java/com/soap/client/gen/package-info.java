c58fd7819d3e5b1527c90c18cb4aeefdf799b54f
Miche <Miche@DESKTOP-535NINS>
2023-12-19 12:40:15 +0100

    .

--- Calcolatrice/src/main/java/com/soap/client/gen/package-info.java
+++ Calcolatrice/src/main/java/com/soap/client/gen/package-info.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.3.0 
 // Vedere <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2023.12.01 alle 02:51:10 PM CET 
+// Generato il: 2023.12.19 alle 12:39:25 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://tempuri.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
